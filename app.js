6b7b717a858a837cef92cbefcb33cf2f1eb6c85a
Dallin Hale <dallinhale@gmail.com>
2025-05-15 19:51:33 -0600

    adding origin to lock down requests to only those 2 sites

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-/* The actual app and middleware placed here */ 
+/* The actual app and middleware placed here */
 
-const express = require('express');
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://three41project-f9tj.onrender.com",
+      "https://cse341-contacts-frontend.netlify.app",
+      "http://localhost:3000",
+    ],
+  })
+);
 app.use(express.json()); // parse incoming JSON
 
-app.use('/', require('./routes/index'));
+app.use("/", require("./routes/index"));
 
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({error: 'There was an error' });
+  console.error(err.stack);
+  res.status(500).json({ error: "There was an error" });
 });
 
 module.exports = app;
